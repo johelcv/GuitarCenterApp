7c0508e538e455ea2f7da8644860b2014cb79710
johelcv <johelcv@gmail.com>
2016-01-09 01:51:10 -0500

    Crear BL

---
+++ GuitarCenter.BL/GuitarService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuitarCenter.Entities;
+
+namespace GuitarCenter.BL
+{
+    public class GuitarService : IGuitarService
+    {
+        private DAC.IGuitarRepositorio GuitarRepositorio = new DAC.GuitarRepository();
+
+        public void Create(Guitar Model)
+        {
+            this.GuitarRepositorio.Create(Model);
+        }
+
+        public void CreateReview(Review Model)
+        {
+            this.GuitarRepositorio.CreateReview(Model);
+        }
+
+        public IEnumerable<Guitar> GetAll()
+        {
+            return this.GuitarRepositorio.GetAll();
+        }
+
+        public Guitar GetOne(int Id)
+        {
+            return this.GuitarRepositorio.GetOne(Id);
+        }
+    }
+}
---
+++ GuitarCenter.BL/IGuitarService.cs
@@ -0,0 +1,17 @@
+﻿using GuitarCenter.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarCenter.BL
+{
+    public interface IGuitarService
+    {
+        Guitar GetOne(int Id);
+        IEnumerable<Guitar> GetAll();
+        void Create(Guitar Model);
+        void CreateReview(Review Model);
+    }
+}
