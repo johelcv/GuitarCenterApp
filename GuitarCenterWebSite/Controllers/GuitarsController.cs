b5d33e3296f4368a923702391d83698811b1a81a
johelcv <johelcv@gmail.com>
2016-01-09 02:03:04 -0500

    Confirmar

--- GuitarCenterWebSite/Controllers/GuitarsController.cs
+++ GuitarCenterWebSite/Controllers/GuitarsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuitarCenter.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,41 @@ using System.Web.Mvc;
 
 namespace GuitarCenterWebSite.Controllers
 {
+    [Authorize]
     public class GuitarsController : Controller
     {
+        private GuitarCenter.BL.IGuitarService GuitarService = new GuitarCenter.BL.GuitarService();
         // GET: Guitars
         public ActionResult Index()
         {
-            return View();
+            var model = this.GuitarService.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = this.GuitarService.GetOne(id);
+            return this.View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(Guitar Model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.GuitarService.Create(Model);
+                return this.RedirectToAction("Index");
+            }
+            else
+            {
+                return View(Model);
+            }
         }
     }
 }
