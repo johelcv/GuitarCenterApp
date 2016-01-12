d711568f60b2e1a9207a9a0c26865cc5c6914c79
johelcv <johelcv@gmail.com>
2016-01-12 08:08:34 -0500

    Comit

--- GuitarCenterWebSite/Models/IdentityModels.cs
+++ GuitarCenterWebSite/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace GuitarCenterWebSite.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Guitars", throwIfV1Schema: false)
         {
         }
 
